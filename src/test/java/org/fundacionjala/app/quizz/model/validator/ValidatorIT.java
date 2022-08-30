2f9935726bb04273583b2270cf904cabb7ca6ab8
catOWO2011 <catOWO2011@gmail.com>
2022-08-30 18:54:33 -0400

    Task 8 Include UpperCase scenarios

--- src/test/java/org/fundacionjala/app/quizz/model/validator/ValidatorIT.java
+++ src/test/java/org/fundacionjala/app/quizz/model/validator/ValidatorIT.java
@@ -1,10 +1,10 @@
 package org.fundacionjala.app.quizz.model.validator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ValidatorIT {
 
@@ -17,5 +17,39 @@ public class ValidatorIT {
 
         Assert.assertEquals(DateValidator.class, dateValidator.getClass());
         Assert.assertTrue(errors.isEmpty());
+    }
+
+    @Test
+    public void testDateValidatorWithWrongFormat() {
+        List<String> errors = new ArrayList<>();
+        Validator dateValidator = ValidatorType.DATE.getValidator();
+
+        dateValidator.validate("24123/12231/2021", null, errors);
+
+        Assert.assertEquals(DateValidator.class, dateValidator.getClass());
+        Assert.assertFalse(!errors.isEmpty());
+    }
+
+    @Test
+    public void testUpperCaseValidatorWithCorrectValue() {
+        List<String> errors = new ArrayList<>();
+        Validator upperCaseValidator = ValidatorType.UPPER_CASE_VALIDATOR.getValidator();
+
+        upperCaseValidator.validate("THEFOXJUMPSOVERTHELAZYDOG", null, errors);
+
+        Assert.assertEquals(UpperCaseValidator.class, upperCaseValidator.getClass());
+        Assert.assertTrue(!errors.isEmpty());
+    }
+
+    @Test
+    public void testUpperCaseValidatorWithWrongValue() {
+        List<String> errors = new ArrayList<>();
+        Validator upperCaseValidator = ValidatorType.UPPER_CASE_VALIDATOR.getValidator();
+
+        upperCaseValidator.validate("TheFoxJumpsOverTheLazyDog", null, errors);
+        System.out.println("TheFoxJumpsOverTheLazyDog".matches("/^[A-Z]+$/g"));
+
+        Assert.assertEquals(UpperCaseValidator.class, upperCaseValidator.getClass());
+        Assert.assertTrue(!errors.isEmpty());
     }
 }
